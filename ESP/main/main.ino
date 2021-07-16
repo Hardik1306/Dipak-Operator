61db432ef60b0c32247b775d68342858857aed79
Hardik1306 <hardikpkapadia13@gmail.com>
2021-07-17 04:22:22 +0530

    first

---
+++ ESP/main/main.ino
@@ -0,0 +1,146 @@
+#include "main.h"
+
+
+void setup() {
+  pinMode(5, OUTPUT);
+  vspi = new SPIClass(VSPI);
+  vspi->begin();
+  Ps3.begin("44:44:44:44:44:44");
+
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
+
+  Serial.begin(115200);
+  //  Serial1.begin(115200, SERIAL_8N1, 13, 14);
+  Serial2.begin(115200);
+
+  delay(1000);
+  cytron_1.send_dummy_bit();
+  delay(1000);
+  millis();
+  //  bot.callibrate();
+  //  pinMode(LIMIT1, INPUT);
+  //  pinMode(LIMIT2, INPUT);
+
+  //  tfmini.begin(&Serial);
+}
+
+uint8_t pwm = 70;
+bool prev_pwm = false;
+bool l1 = 0;
+bool r1 = 0;
+void loop() {
+  mpu6050.update();
+  Serial.println(mpu6050.getAngleZ());
+  if (Ps3.isConnected()) {
+    if (Ps3.data.button.l1 && Ps3.data.button.r1) {
+      pwm = 65;
+    }
+    else if (Ps3.data.button.l1 && !l1)
+    {
+      pwm = pwm - 5;
+      l1 = 1;
+    }
+    else if (Ps3.data.button.l1);
+    else if (Ps3.data.button.r1 && !r1)
+    {
+      pwm = pwm + 10;
+      r1 = 1;
+    }
+    else if (Ps3.data.button.r1);
+    else if (Ps3.data.button.l2)
+    {
+      bot.base_anti_clockwise(pwm, pwm, pwm, pwm);
+    }
+    else if (Ps3.data.button.r2)
+    {
+      bot.base_clockwise(pwm, pwm, pwm, pwm);
+    }
+    else if (Ps3.data.button.up && Ps3.data.button.right)
+    {
+      bot.up_right(pwm, pwm, pwm, pwm);
+    }
+    else if (Ps3.data.button.up && Ps3.data.button.left)
+    {
+      bot.up_left(pwm, pwm, pwm, pwm);
+    }
+    else if (Ps3.data.button.down && Ps3.data.button.right)
+    {
+      bot.down_right(pwm, pwm, pwm, pwm);
+    }
+    else if (Ps3.data.button.down && Ps3.data.button.left)
+    {
+      bot.down_left(pwm, pwm, pwm, pwm);
+    }
+    else if (Ps3.data.button.up)
+    {
+      bot.forward(pwm, pwm, pwm, pwm);
+    }
+    else if (Ps3.data.button.down)
+    {
+      bot.backward(pwm, pwm, pwm, pwm);
+    }
+    else if (Ps3.data.button.left)
+    {
+      bot.left(pwm, pwm, pwm, pwm);
+    }
+    else if (Ps3.data.button.right)
+    {
+      bot.right(pwm, pwm, pwm, pwm);
+    } else if (Ps3.data.button.right) {
+      bot.calib_angle();
+    }
+    else if (Ps3.data.button.right) {
+      bot.calib_angle();
+    }
+    else if (Ps3.data.button.triangle) {
+      send_data(1);
+    }
+    else if (Ps3.data.button.circle) {
+      send_data(5);
+    }
+    else if (Ps3.data.button.cross) {
+      send_data(10);
+    }
+    else if (Ps3.data.button.square) {
+      send_data(15);
+    }
+    else if (Ps3.data.analog.stick.ly < -50 ) {
+      send_data(30);
+    }
+    else if (Ps3.data.analog.stick.ly > 50 ) {
+      send_data(35);
+    }
+    else if (Ps3.data.button.select) {
+      send_data(40);
+    }
+    else if (Ps3.data.button.start) {
+      send_data(45);
+    }
+    else if (Ps3.data.button.r3) {
+      send_data(50);
+    }
+    else if (Ps3.data.button.l3) {
+      send_data(55);
+    }
+    else
+    {
+      l1 = 0;
+      r1 = 0;
+      prev_pwm = false;
+      bot.base_free();
+      send_data(0);
+    }
+  } else {
+    bot.base_free();
+    send_data(0);
+  }
+}
+void send_data(byte x) {
+  vspi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
+  digitalWrite(5, LOW);
+  vspi->transfer(x);
+  digitalWrite(5, HIGH);
+  vspi->endTransaction();
+}
---
+++ MEGA/main/main.ino
@@ -0,0 +1,169 @@
+#include <SPI.h>
+#include "TF.h"
+#include "Adafruit_MCP23017.h"
+#include <Adafruit_MCP4725.h>
+#include "Motor.h"
+#include"cytron.h"
+#include <Encoder.h>
+Adafruit_MCP4725 dac;
+Adafruit_MCP23017 mcp;
+Motor pipe_motor(35,37, 7);
+
+int buttonstate = 0;
+int lastbuttonstate = 0;
+
+bool r3 = 0;
+bool circle = 0;
+bool Square = 0;
+bool cross = 0;
+bool triangle = 0;
+bool start = 0;
+bool select = 0;
+bool pipe_pnumetic = 0;
+byte pipe;
+uint8_t button = 7;
+unsigned long previousMillis = 0;
+unsigned long previousMillis1 = 0;
+const long interval = 500;
+int pressure;
+unsigned long currentMillis;
+unsigned long currentMillis1;
+
+
+
+ISR(SPI_STC_vect) {
+  button = SPDR;
+
+}
+
+void setup() {
+  pinMode(39, INPUT);
+  Serial.begin(115200);
+  Serial.println("low");
+  mcp.begin();
+  SPCR |= _BV(SPE);
+  SPI.attachInterrupt();
+  Serial.println("high");
+  for (uint8_t i = 1; i < 16; i++) {
+    mcp.pinMode(i, OUTPUT);
+    //        mcp.digitalWrite(i, HIGH);
+  }
+  dac.begin(0x60);
+  dac.setVoltage(0, false);
+
+}
+
+
+void loop() {
+  Serial.println(button);
+    if (button == 55 && !r3) {
+    r3 = 1;
+    pipe_pnumetic = !pipe_pnumetic;
+    mcp.digitalWrite(7, pipe_pnumetic);
+    mcp.digitalWrite(8, !pipe_pnumetic);
+  }
+  else if (button == 55);
+  else if (button == 1 && !triangle) {
+    triangle = 1;
+    high(2);
+    delay(500);
+    low(2);
+  }
+  else if (button == 1);
+  else if (button == 5 && !circle) {
+    circle = 1;
+    high(3);
+    delay(500);
+    low(3);
+  }
+  else if (button == 5);
+  else if (button == 10 && !cross) {
+    cross = 1;
+    high(4);
+    delay(500);
+    low(4);
+  }
+  else if (button == 10);
+  else if (button == 15 && !Square) {
+    Square = 1;
+    high(5);
+    delay(500);
+    low(5);
+  }
+  else if (button == 15);
+  else if (button == 50 && !r3) {
+    r3 = 1;
+    high(6);
+    delay(500);
+    low(6);
+  }
+  else if (button == 50);
+
+  else if (button == 45 && !start) {
+    pressure = pressure + 163;
+    dac.setVoltage(pressure, false);
+    start = 1;
+    currentMillis = millis();
+    previousMillis = currentMillis;
+  }
+  else if (button == 45) {
+    currentMillis = millis();
+    if ((currentMillis - previousMillis) >= interval) {
+      pressure = pressure + 15;
+      pressure = constrain(pressure , 0, 4095);
+      dac.setVoltage(pressure, false);
+      Serial.println(pressure);
+      delay(10);
+    }
+  }
+  else if (button == 40 && !select) {
+    pressure = pressure - 163;
+    dac.setVoltage(pressure, false);
+    select = 1;
+    currentMillis1 = millis();
+    previousMillis1 = currentMillis1;
+  }
+  else if (button == 40) {
+    currentMillis1 = millis();
+    if ((currentMillis1 - previousMillis1) >= interval) {
+      pressure = pressure - 15;
+      pressure = constrain(pressure , 0, 4095);
+      dac.setVoltage(pressure, false);
+      Serial.println(pressure);
+      delay(10);
+    }
+  }
+  else if (button == 30 && !digitalRead(A6)) {
+    pipe_motor.anti_clockwise(50);
+  }
+  else if (button == 35) {
+    pipe_motor.clockwise(50);
+  }
+  else {
+    pipe_motor.brake();
+    r3 = 0;
+    cross = 0;
+    triangle = 0;
+    Square = 0;
+    circle = 0;
+    select = 0;
+    start = 0;
+  }
+}
+void high(uint8_t pin) {
+  mcp.digitalWrite(pin, HIGH);
+}
+void low(uint8_t pin) {
+  mcp.digitalWrite(pin, LOW);
+}
+void dac_set_voltage(float y) {
+  int z = (y * 4095) / 5.07;
+  dac.setVoltage(z, false);
+}
+//void throw_arrow() {
+//  high(pipe + 1);
+//  delay(800);
+//  low(pipe + 1);
+//  if (pipe == 5 )
+//    pipe = 0;
+//}
